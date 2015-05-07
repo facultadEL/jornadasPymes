94de360e9ada42cdd8e27d11a8a06263f17cca9b
Eze BIT <efigueroa@bit.com.ar>
2015-05-07 10:04:10 -0300

    Modificado envio mail

--- guardarIncripcionJornada.php
+++ guardarIncripcionJornada.php
@@ -101,12 +101,12 @@ $id_Inscripto = $_REQUEST['idInscripto'];
 			<strong>Te has inscripto correctamente a las Jornadas para PyMEs de la UTN</strong>, que se llevarán a cabo los días miércoles 3 y jueves 4 de junio de 2015 a partir de las 14:00 hs. 
 			en la UTN de Villa María.<br/><br/>
 
-			Te invitamos a conocer la agenda del evento y todos los detalles en <a>www.jornadaspymesutn.com.ar</a> <br/><br/>
+			Te invitamos a conocer la agenda del evento y todos los detalles en <a href=".'"www.jornadaspymesutn.com.ar"'.">www.jornadaspymesutn.com.ar</a> <br/><br/>
 
 			Recuerda asistir con tu DNI para que podamos entregarte tú certificado.<br/>
 			También te recomendamos llevar tarjetas de presentación para sacar el máximo provecho de esta oportunidad única para ponerte en contacto con otros emprendedores y empresarios.<br/><br/>
 
-			* Por favor, en caso de no poder asistir, notifícanos con tiempo a <a>info@jornadaspymesutn.com.ar</a><br/><br/>
+			* Por favor, en caso de no poder asistir, notifícanos con tiempo a <a href=".'"mailto:info@jornadaspymesutn.com.ar" target="_top"'.">info@jornadaspymesutn.com.ar</a><br/><br/>
 
 			<strong>¡Te esperamos!</strong>
 
