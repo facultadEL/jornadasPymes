72453c9a90b21fd37fce0df83d37190e5cd6c798
Eze Olea Figueroa <eze.olea.f@gmail.com>
2015-05-08 15:32:11 -0300

    Modificadas las referencias dentro de la libreria

--- libreria.php
+++ libreria.php
@@ -1,5 +1,5 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type='text/javascript' src="jquery.min-1.9.1.js"></script>
 <?php
 //Actualizada a la fecha 30/09/2014 
 //error_reporting(E_ALL);
