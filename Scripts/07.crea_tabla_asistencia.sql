aa6d36a574cbd9860823971e6c7900a602fb9814
Lucas <lpm19.2009@gmail.com>
2015-05-28 00:21:24 -0300

    script para tomar la asistencia, si le falta algo agregalo

---
+++ Scripts/07.crea_tabla_asistencia.sql
@@ -0,0 +1,17 @@
+--Poner en TRUE a las actividades que asistió
+--guardar el ID del inscripto
+
+CREATE TABLE asistencia(
+  id serial NOT NULL primary key,
+  id_inscripto integer NOT NULL,
+  actividad1 boolean default false,
+  actividad2 boolean default false,
+  actividad3 boolean default false,
+  actividad4 boolean default false,
+  actividad5 boolean default false,
+  actividad6 boolean default false,
+  actividad7 boolean default false,
+  actividad8 boolean default false,
+  actividad9 boolean default false,
+  actividad10 boolean default false
+);
